fbbf142ca208494a994d7d4527ebc4c0354d4414
bitdeep <19378023+bitdeep@users.noreply.github.com>
2023-07-20 14:37:06 -0300

    add discord notification

--- src/index.js
+++ src/index.js
@@ -10,23 +10,59 @@ const fs = require('fs')
 const chalk = require('chalk');
 let running = false;
 const cacheFile = '/tmp/tx.cache';
+
+const { Client, GatewayIntentBits, ActivityType, bold, codeBlock, blockQuote, italic } = require('discord.js');
+
+const discordConfig = {
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+    ]
+};
+const discord = new Client(discordConfig);
+let discordReady = false, discordChannel;
+const discordToken = process.env.DISCORD_TOKEN;
+const discordChannelId = process.env.DISCORD_CHANNEL_ID;
+async function discordSend(msg){
+    if( ! msg ) return;
+    if( discordReady ) {
+        try {
+            await discordChannel.send(msg);
+        }catch(e){
+            console.log(`Discord Send Error: ${e.toString()}`);
+        }
+    }
+}
+function discordStatus(msg){
+    if( ! msg ) return;
+    if( discordReady )
+        discord.user?.setActivity(msg, { type: ActivityType.Watching })
+    else
+        console.log(`Discord Status: ${msg}`);
+}
 const magenta = function () {
     console.log(chalk.magenta(...arguments))
+    discordSend(bold(...arguments));
 };
 const cyan = function () {
     console.log(chalk.cyan(...arguments))
+    discordSend(bold(...arguments));
 };
 const yellow = function () {
     console.log(chalk.yellow(...arguments))
+    discordSend(blockQuote(...arguments));
 };
 const red = function () {
     console.log(chalk.red(...arguments))
+    discordSend(codeBlock(...arguments));
 };
 const blue = function () {
     console.log(chalk.blue(...arguments))
+    discordSend(italic(...arguments));
 };
 const green = function () {
     console.log(chalk.green(...arguments))
+    discordSend(...arguments);
 };
 
 const Web3 = require('web3');
@@ -48,7 +84,7 @@ function getNonce() {
 let txCache = {};
 async function distro() {
     try {
-        if (running) return red(`Stop: already running, waiting loop to finish...`);
+        if (running) return yellow(`Wait: already running, waiting loop to finish...`);
         running = true;
         baseNonce = web3.eth.getTransactionCount(addressOfKey);
         const length = await voter.methods.length().call();
@@ -62,12 +98,11 @@ async function distro() {
                 await distribute(i, gaugeAddress, symbol);
             } catch (e) {
                 red(` - ${i} of ${length}) ${e.toString()}`);
-                console.log(e);
             }
         }
         running = false;
     }catch(e){
-        console.log(e);
+        red(`Distro: ${e.toString()}`);
         running = false;
     }
 }
@@ -79,7 +114,11 @@ async function distribute(i, gaugeAddress, symbol){
         txCache[latestEpoch] = txCache[latestEpoch] || {};
 
         let tx = txCache[latestEpoch][gaugeAddress];
-        if( tx) return yellow(` -- Skip: ${tx}`);
+        if( tx){
+            yellow(` -- Skip: ${tx}`);
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+            return;
+        }
 
         const distroTx = voter.methods.distribute(gaugeAddress);
         // calculate gas price plus 10%
@@ -122,7 +161,9 @@ function getEpochStart(timestamp) {
 function getEpoch(currentTimeStamp) {
     const startBlockTimestamp = parseInt(process.env.START_BLOCK_TIMESTAMP);
     const currentEpoch = parseInt(getEpochStart(currentTimeStamp));
-    return parseInt((currentEpoch - startBlockTimestamp) / SEVEN_DAYS);
+    const _epoch = parseInt((currentEpoch - startBlockTimestamp) / SEVEN_DAYS);
+    discordStatus(`Equilibre Distro #${_epoch}`);
+    return _epoch;
 }
 
 let latestEpoch;
@@ -132,8 +173,7 @@ async function run(){
         const r = await web3.eth.getBlock("latest");
         timestamp = r.timestamp;
     }catch(e){
-        red(`Stop (can't get last block): ${e.toString()}`)
-        return ;
+        return red(`Stop (can't get last block): ${e.toString()}, retry in 1 minute.`);
     }
     const epoch = getEpoch(timestamp);
     if( ! latestEpoch ){
@@ -148,7 +188,8 @@ async function run(){
 }
 
 let addressOfKey;
-async function main() {
+
+async function app() {
     // sleep 1day
     //await new Promise((resolve) => setTimeout(resolve, ONE_DAY));
     // create cacheFile if not exists:
@@ -159,18 +200,50 @@ async function main() {
     txCache = txCache || {};
 
     if( ! process.env.CONTRACT ){
-        return new Error(".env not found!");
+        return red("STOP: .env not found!");
     }else{
         addressOfKey = web3.eth.accounts.privateKeyToAccount(process.env.PRIVATE_KEY_ADMIN).address;
         const balanceInWei = await web3.eth.getBalance(account);
         const balance = web3.utils.fromWei(balanceInWei, 'ether');
-        blue(`Contract: ${process.env.CONTRACT}`);
-        blue(`RPC: ${process.env.RPC}`);
-        blue(`Admin: ${addressOfKey}`);
-        blue(`Balance: ${balance} KAVA`);
+        let info = `Contract: ${process.env.CONTRACT}`;
+        info += `\nUsing RPC: ${process.env.RPC}`;
+        info += `\nBot Wallet: ${addressOfKey}`;
+        info += `\nBalance: ${balance} KAVA`;
+        blue(info);
+        run();
+        setInterval(run, 60 * 1000 );
     }
-    await run();
-    setInterval(run, 60 * 1000 );
+}
+
+async function main() {
+    discord.on('ready', async () => {
+        green(`Logged in as ${discord.user.tag}!`);
+
+        discordChannel = discord.channels.cache.find(i => i.name === discordChannelId);
+        if (!discordChannel) {
+            return red(`Discord: [STOP] channel not found "${discordChannelId}"`);
+        }
+
+        discordReady = true;
+        blue(`Voter distro online #${discordChannel.name}`);
+
+
+        discordStatus(`Equilibre Distro...`);
+        discord.user.setStatus('invisible');
+        app();
+
+    });
+
+    if( ! discordToken ) {
+        return red("Discord: [STOP] token not found!");
+    }
+
+    try {
+        await discord.login(discordToken);
+    } catch (e) {
+        console.log(`LOGIN ERROR: ${e.toString()}`);
+    }
+
 }
 
 main();
